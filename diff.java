6ddcf7d640c9ea8aa4535ffefb186649238d769b
Nitseh Singh <niteshsingh5375@gmail.com>
2021-09-07 12:43:44 +0530

    new implementaiton

--- diff.java
+++ diff.java
@@ -3,7 +3,7 @@ public class sum{
     public static void main(String[] args) {
         int a=23;
         int b=34;
-        System.out.println(a-b);
+        System.out.println(a-b*b);
         
     }
 }
